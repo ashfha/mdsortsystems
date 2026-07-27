38a5aedcedb5a628a72d75323136d8564a344e5d
ashfha <mdsortsystemorignal@gmail.com>
2026-07-27 19:50:48 +0300

    Simplify app routes to the new TravelMatch experience

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ThemeProvider from "@/components/ThemeProvider";
-import Index from "./pages/Index.tsx";
-import Login from "./pages/Login.tsx";
-import ResetPassword from "./pages/ResetPassword.tsx";
-import Dashboard from "./pages/Dashboard.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -21,11 +17,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
+            <Route path="*" element={<Index />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
