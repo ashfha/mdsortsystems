b68047cd3fda8922dd415dd899cb288cc888e918
ashfha <mdsortsystemorignal@gmail.com>
2026-07-28 10:38:01 +0300

    Split the homepage into a lazy-loaded route to reduce the initial bundle size

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { lazy, Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import ThemeProvider from "@/components/ThemeProvider";
-import Index from "./pages/Index";
+import ThemeProvider from "./providers/ThemeProvider";
 
+const Index = lazy(() => import("./pages/Index"));
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -15,10 +16,12 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<Index />} />
-          </Routes>
+          <Suspense fallback={null}>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="*" element={<Index />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
