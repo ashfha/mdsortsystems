8365e8e0a4de552d684d148581f9c3c2f7d7c85c
ashfha <mdsortsystemorignal@gmail.com>
2026-07-29 14:15:12 +0300

    Switch travel app to premium multi-page routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "./providers/ThemeProvider";
-import Index from "./pages/Index";
+import {
+  AccountPage,
+  DestinationPage,
+  FavoritesPage,
+  HomePage,
+  PlannerPage,
+  ResultsPage,
+  SearchPage,
+} from "./pages/TravelPlatform";
 
 const queryClient = new QueryClient();
 
@@ -16,8 +24,14 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<Index />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/results" element={<ResultsPage />} />
+            <Route path="/destination/:id" element={<DestinationPage />} />
+            <Route path="/planner" element={<PlannerPage />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+            <Route path="/account" element={<AccountPage />} />
+            <Route path="*" element={<HomePage />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
